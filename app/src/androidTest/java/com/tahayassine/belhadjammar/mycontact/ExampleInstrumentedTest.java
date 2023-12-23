193eeb59dbbccb95d05b05ea32b5e7ea26046102
GodTahaYassine <88496980+GodTahaYassine@users.noreply.github.com>
2023-12-23 03:31:15 +0100

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/tahayassine/belhadjammar/mycontact/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tahayassine/belhadjammar/mycontact/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.jovanovic.stefan.sqlitetutorial", appContext.getPackageName());
+        assertEquals("com.tahayassine.belhadjammar.mycontact", appContext.getPackageName());
     }
 }
